6b707e37b4c5719e387e786ce365cc201db65abb
mesterum <mesterum@users.noreply.github.com>
2023-11-07 10:19:53 +0000

    Deploying to gh-pages from @ chiperandrei42/parcel-project-team5@950a7004f7411eeafa217c542cf3199c82fb77dc 🚀

---
+++ index.f5a02d11.js
@@ -0,0 +1,3 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),closeModalLink:document.querySelector(".modal-nav"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.closeModalLink.addEventListener("click",o)})();//# sourceMappingURL=index.f5a02d11.js.map
+
+//# sourceMappingURL=index.f5a02d11.js.map
